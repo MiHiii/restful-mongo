f0df109629e482273191cf88667ead1b8fdc5987
MiHiii <hieupham08111@gmail.com>
2024-07-18 19:34:08 +0700

    install dotenv

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
-const port = 3000;
+require('dotenv').config();
 
-//config template engine
+const app = express();
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
+
+// console.log('>>check env: ', process.env);
+
+//config template engineF
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -17,6 +22,6 @@ app.get('/test', (req, res) => {
   res.render('sample.ejs');
 });
 
-app.listen(3000, () => {
+app.listen(port, hostname, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
