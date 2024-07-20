3983918fbc2ff6d0d6b4fe1a995c652310923e82
MiHiii <hieupham08111@gmail.com>
2024-07-20 13:10:33 +0700

    User mysql with Nodejs

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const configViewEngine = require('./config/viewengine');
 const app = express();
 const webRoutes = require('./routes/web');
+
+const mysql = require('mysql2');
 
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
@@ -15,6 +16,22 @@ configViewEngine(app);
 
 //init router
 app.use('/', webRoutes);
+
+//test connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307, //default port is 3306
+  user: 'root',
+  database: 'mihi-db',
+  password: '123456',
+});
+connection.query(
+  'SELECT * FROM `Users` WHERE `name` = "mihi"',
+  function (err, results, fields) {
+    console.log('>>results: ', results); // results contains rows returned by server
+    // console.log('>>fields: ', fields); // fields contains extra meta data about results, if available
+  },
+);
 
 app.listen(port, hostname, () => {
   console.log(`Server Is running on http://localhost:${port}`);
