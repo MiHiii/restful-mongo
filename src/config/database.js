171e47d22e2d66796df67a17e2a4565a582019c7
MiHiii <hieupham08111@gmail.com>
2024-07-28 22:48:49 +0700

    test

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
-// //Connection pool
-// const connection = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT, //default port is 3306
-//   user: process.env.DB_USER,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
+//Connection pool
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT, //default port is 3306
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 module.exports = connection;
